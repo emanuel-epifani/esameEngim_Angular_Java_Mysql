a962dd7376347da5a7025cd39fd105f7d1f07bc7
emanuel-epifani <emanuel.epifani.m@gmail.com>
2022-07-20 09:57:00 +0200

    first commit

---
+++ be_java_esameFinale/src/main/java/com/example/be_java_esamefinale/BeJavaEsameFinaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_java_esamefinale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJavaEsameFinaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeJavaEsameFinaleApplication.class, args);
+    }
+
+}
---
+++ be_java_esameFinale/src/test/java/com/example/be_java_esamefinale/BeJavaEsameFinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_java_esamefinale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJavaEsameFinaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
