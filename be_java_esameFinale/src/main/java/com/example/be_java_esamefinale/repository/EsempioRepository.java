35e1379174b9baa159737048a93aeaa5b6ebd51e
emanuel-epifani <emanuel.epifani.m@gmail.com>
2022-07-20 16:18:26 +0200

    struttura BE impostata e FE funzionante

--- be_java_esameFinale/src/main/java/com/example/be_java_esamefinale/repository/EsempioRepository.java
+++ be_java_esameFinale/src/main/java/com/example/be_java_esamefinale/repository/EsempioRepository.java
@@ -4,58 +4,58 @@ public class EsempioRepository {
 
     //select SENZA prepare statement (senza variabile al suo interno)
 
-    //    public static List<Fermata> getFermate(){
-    //        // creo una lista da restituire
-    //        List<Fermata> fermate = new ArrayList<>();
-    //        try{
-    //            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-    //            PreparedStatement stmt = conn.prepareStatement("select id, nome, zona from fermate");
-    //            ResultSet rs = stmt.executeQuery();
-    //
-    //            while(rs.next()){
-    //
-    //                // creo una fermata con i dati del DB
-    //                Fermata fermata = new Fermata(
-    //                        rs.getInt("id"),
-    //                        rs.getString("nome"),
-    //                        rs.getString("zona"));
-    //
-    //                // aggiungo alla lista
-    //                fermate.add(fermata);
-    //            }
-    //
-    //        } catch (SQLException e) {
-    //            e.printStackTrace();
-    //        }
-    //        //restituisco la lista
-    //        return fermate;
-    //    }
+//        public static List<Fermata> getFermate(){
+//            // creo una lista da restituire
+//            List<Fermata> fermate = new ArrayList<>();
+//            try{
+//                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+//                PreparedStatement stmt = conn.prepareStatement("select id, nome, zona from fermate");
+//                ResultSet rs = stmt.executeQuery();
+//
+//                while(rs.next()){
+//
+//                    // creo una fermata con i dati del DB
+//                    Fermata fermata = new Fermata(
+//                            rs.getInt("id"),
+//                            rs.getString("nome"),
+//                            rs.getString("zona"));
+//
+//                    // aggiungo alla lista
+//                    fermate.add(fermata);
+//                }
+//
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//            //restituisco la lista
+//            return fermate;
+//        }
 
 
 
-    
+
     //select CON prepare statement (con variabile al suo interno)
 
-    //    private static Fermata getFermataById(int numero_fermata) {
-    //        // dichiaro l'oggetto da restituire
-    //        Fermata fermata = null;
-    //        try{
-    //            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-    //            PreparedStatement stmt = conn.prepareStatement("select id,nome,zona from fermate where id = ? ");
-    //            stmt.setInt(1,numero_fermata);
-    //            ResultSet rs = stmt.executeQuery();
-    //
-    //            while(rs.next()){
-    //                // istanzio l'oggetto da restituire preso dal DB
-    //                fermata = new Fermata(rs.getInt("id"),rs.getString("nome"),rs.getString("zona"));
-    //            }
-    //
-    //        } catch (SQLException e) {
-    //            e.printStackTrace();
-    //        }
-    //        //restituisco la Fermata
-    //        return fermata;
-    //    }
+//        private static Fermata getFermataById(int numero_fermata) {
+//            // dichiaro l'oggetto da restituire
+//            Fermata fermata = null;
+//            try{
+//                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+//                PreparedStatement stmt = conn.prepareStatement("select id,nome,zona from fermate where id = ? ");
+//                stmt.setInt(1,numero_fermata);
+//                ResultSet rs = stmt.executeQuery();
+//
+//                while(rs.next()){
+//                    // istanzio l'oggetto da restituire preso dal DB
+//                    fermata = new Fermata(rs.getInt("id"),rs.getString("nome"),rs.getString("zona"));
+//                }
+//
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//            //restituisco la Fermata
+//            return fermata;
+//        }
 
 
 }
--- fe_angular_esameFinale/src/app/components/prova/prova.component.ts
+++ fe_angular_esameFinale/src/app/components/prova/prova.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-prova',
@@ -6,12 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prova.component.scss']
 })
 export class ProvaComponent implements OnInit {
+  users: User[] = [];
+  missingData: boolean = true;
 
-  constructor() { }
+  constructor(
+  /*     private formBuilder: FormBuilder*/  
+  ) { }
 
   ngOnInit(): void {
     this.getUserFromRequest();
+
   }
+  
+
 
   getUserFromRequest() {
     fetch('https://reqres.in/api/users')
@@ -21,10 +31,39 @@ export class ProvaComponent implements OnInit {
       })
       .then((data)=>{
         console.log(data)
+        console.log(data['data'])
+        this.users = data['data'];
+        console.log("users");
+        console.log(this.users);
       })
       .catch((error)=>{
         console.log('Errore ', error)
       })
+  }
+
+  addNewUser(
+    email: string,
+    firstName: string,
+    last_name: string,
+    avatar: string
+    ) {
+
+      let user: User = {
+      id: this.users.length+1,
+      email: email.trim(),
+      first_name: firstName.trim(),
+      last_name: last_name.trim(),
+      avatar: avatar.trim(),
+    }
+    if(user.email != "" && user.first_name != "" && user.last_name != "" && user.avatar != "") {
+      this.missingData = false;
+      this.users.push(user);
+      console.log(this.users);
+    } else {
+      alert("Dievi riempire tuti i campi per poter salvare")
+      this.missingData = true;
+    }
+
   }
   
 
